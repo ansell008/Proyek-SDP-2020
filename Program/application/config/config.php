572723b24e17fa34d0ec66172b1f1858d35d7058
RommyChristensen <32613677+RommyChristensen@users.noreply.github.com>
2020-03-07 16:31:15 +0700

    commit 2

--- Program/application/config/config.php
+++ Program/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sdp-proyek/';
+$config['base_url'] = 'http://localhost/Github/sdp-proyek/Proyek-SDP-2020/Program/';
 
 /*
 |--------------------------------------------------------------------------
