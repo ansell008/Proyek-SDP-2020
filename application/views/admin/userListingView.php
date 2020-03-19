be1b532e6df5c5751fc05a9e330682ee2de6dadf
RommyChristensen <32613677+RommyChristensen@users.noreply.github.com>
2020-03-19 09:36:52 +0700

    update userlistingview

--- application/views/admin/userListingView.php
+++ application/views/admin/userListingView.php
@@ -157,8 +157,51 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        <div class="row">
-          
+      <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">List Freelancer</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <table class="table table-bordered table-striped" id="tableSkill">
+                  <thead>
+                    <th>Id</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Username</th>
+                    <th>KTP</th>
+                    <th>Action</th>
+                  </thead>
+                  <tbody id="tbSkillData">
+                    
+                  </tbody>
+                  <tfoot>
+                    <th>Id</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Username</th>
+                    <th>KTP</th>
+                    <th>Action</th>
+                  </tfoot>
+                </table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                <button id='addNewSkill' class='btn btn-flat btn-success'>Add New Skill</button>
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+          </div>
         </div>
       </div>
     </section>
