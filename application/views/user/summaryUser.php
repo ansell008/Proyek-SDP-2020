08cf9f8e245699642be7f2e6b88a778f04763593
jennychndr <j.chdr61@gmail.com>
2020-05-03 15:30:37 +0700

    report1

--- application/views/user/summaryUser.php
+++ application/views/user/summaryUser.php
@@ -4,8 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="./week3.js"></script>
+    <script src="./Chart.js"></script>
+    <style>
+        #box {
+        border: 2px solid black;
+        padding: 20px;
+        width: 600px;
+        height: auto;
+        }
+    </style>
 </head>
 <body>
-    <h1>Ini Summary</h1>
+<div id="box" >
+    <canvas id="myChart"></canvas>
+    <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var tahunawal=2018;
+        var tahunakhir=2020;
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: 'Report Jumlah Pelamar Pekerjaan Tahun '+tahunawal+' - '+tahunakhir,
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+        </script>
+    </div>
 </body>
 </html>
